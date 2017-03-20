4187f7429981e34e77521aa55458eb3f8c9058ca
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2017-03-19 20:48:04 -0600

    Se agrega la pantalla Crear Contacto

--- RACM/app/src/main/java/itesm/mx/racm/CrearContacto.java
+++ RACM/app/src/main/java/itesm/mx/racm/CrearContacto.java
@@ -2,6 +2,8 @@ package itesm.mx.racm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CrearContacto extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CrearContacto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_contacto);
+
+        Spinner spinnerCategorias = (Spinner)findViewById(R.id.spinnner_categoriasCC);
+        String[] items = new String[]{"Seleccione la categoría","Familia", "Amigos", "Salud", "Proveedores", "Servicios"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinnerCategorias.setAdapter(adapter);
     }
 }
