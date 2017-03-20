ab93f115aeed5e6861b22fd68226984bce22e01e
Yocelyn <yosy1196@gmail.com>
2017-03-19 18:46:00 -0600

    Prueba Yocelyn

---
+++ RACM/app/src/main/java/itesm/mx/racm/VerFoto.java
@@ -0,0 +1,13 @@
+package itesm.mx.racm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerFoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_foto);
+    }
+}
