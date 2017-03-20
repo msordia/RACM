3f4e0daf5c1ce747aef89f2a484d6a96a0b4a091
EvertSalinas <evertsalinas88@gmail.com>
2017-03-19 19:31:44 -0600

    Revert "Se agrega la pantalla de RegistrarPerfil"
    This reverts commit 15a4bd4ba2857d68d87408e308bfa52ee7861c2b.

--- RACM/app/src/main/java/itesm/mx/racm/MainActivity.java
+++ RACM/app/src/main/java/itesm/mx/racm/MainActivity.java
@@ -3,11 +3,11 @@ package itesm.mx.racm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegistrarPerfil extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registrar_perfil);
+        setContentView(R.layout.activity_main);
     }
 }
