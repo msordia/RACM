0cd7dc7da72f5be91fa24f22bcb0ecc12b911b72
Yocelyn <yosy1196@gmail.com>
2017-03-20 22:38:06 -0600

    intents y cambio en .xml Perfil Detalle

--- RACM/app/src/main/java/itesm/mx/racm/PerfilDetalle.java
+++ RACM/app/src/main/java/itesm/mx/racm/PerfilDetalle.java
@@ -1,13 +1,27 @@
 package itesm.mx.racm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class PerfilDetalle extends AppCompatActivity {
+public class PerfilDetalle extends AppCompatActivity implements View.OnClickListener{
+
+    ImageView ivFotoPD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_detalle);
+
+        ivFotoPD= (ImageView) findViewById(R.id.image_fotoPD);
+        ivFotoPD.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent  intent= new Intent(this,VerFoto.class);
+        startActivity(intent);
     }
 }
