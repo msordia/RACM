fbd058d93678db7740e879c07852dd5c4be9accf
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2017-03-20 21:50:23 -0600

    Agrega intents a Crear y Editar Contacto

--- RACM/app/src/main/java/itesm/mx/racm/EditarContacto.java
+++ RACM/app/src/main/java/itesm/mx/racm/EditarContacto.java
@@ -1,13 +1,50 @@
 package itesm.mx.racm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class EditarContacto extends AppCompatActivity {
+public class EditarContacto extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnGuardar;
+    ImageView imgFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_contacto);
+
+        btnGuardar = (Button) findViewById(R.id.button_guardarEC);
+        btnGuardar.setOnClickListener(this);
+
+        imgFoto = (ImageView) findViewById(R.id.image_fotoEC);
+        imgFoto.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_guardarEC:
+                //Intent a Contacto Detalle
+
+                //Intent intent = new Intent(CrearContacto.this, ContactoDetalle.class);
+                //startActivity(intent);
+
+                break;
+
+            case R.id.image_fotoEC:
+                //Intent a Editar Foto
+
+                Intent intent = new Intent(EditarContacto.this, CambiarFoto.class);
+                startActivity(intent);
+
+                break;
+
+            default:
+                break;
+        }
     }
 }
