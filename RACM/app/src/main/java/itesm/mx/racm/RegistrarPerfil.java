3106c92caba8ab97f5c66a0375a9ac5cebfa074e
Sergio Sánchez <ssanchez@accionti.com>
2017-03-20 22:10:39 -0600

    Archivos para el demo

--- RACM/app/src/main/java/itesm/mx/racm/RegistrarPerfil.java
+++ RACM/app/src/main/java/itesm/mx/racm/RegistrarPerfil.java
@@ -1,13 +1,54 @@
 package itesm.mx.racm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class RegistrarPerfil extends AppCompatActivity {
+public class RegistrarPerfil extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnGuardar, btnDireccion;
+    ImageButton btnImagePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_perfil);
+
+        btnGuardar = (Button) findViewById(R.id.button_guardarRP);
+        btnImagePhoto = (ImageButton) findViewById(R.id.image_fotoRP);
+        btnDireccion = (Button) findViewById(R.id.button_ubicacionRP);
+
+
+        btnGuardar.setOnClickListener(this);
+        btnImagePhoto.setOnClickListener(this);
+        btnDireccion.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_guardarRP:
+                Intent intent = new Intent(this, CrearContacto.class);
+                startActivity(intent);
+                break;
+
+            case  R.id.image_fotoRP:
+                Intent intent2 = new Intent(this, CambiarFoto.class);
+                startActivity(intent2);
+                break;
+
+            case R.id.button_ubicacionRP:
+                Toast.makeText(this.getApplicationContext(), "Activar GPS", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+
     }
 }
