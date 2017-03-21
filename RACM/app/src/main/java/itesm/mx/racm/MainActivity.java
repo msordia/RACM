efa867da9d1309da9adec3aca18905f70fafdeaf
Sergio Sánchez <ssanchez@accionti.com>
2017-03-20 23:11:50 -0600

    Cambios menores para el prototipo

--- RACM/app/src/main/java/itesm/mx/racm/MainActivity.java
+++ RACM/app/src/main/java/itesm/mx/racm/MainActivity.java
@@ -2,12 +2,18 @@ package itesm.mx.racm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_manual);
+    }
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }
