a2a32f7f4e2120c152bf203f48bc118120814f6a
EvertSalinas <evertsalinas88@gmail.com>
2017-04-03 17:08:34 -0500

    Se agrega el boton flotante, la lista de contactos y se modifica el boton de search

--- RACM/app/src/main/java/itesm/mx/racm/MainActivity.java
+++ RACM/app/src/main/java/itesm/mx/racm/MainActivity.java
@@ -2,54 +2,42 @@ package itesm.mx.racm;
 
 import android.content.Intent;
 import android.media.Image;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     ImageButton imageSearch, imageContact, imagePhone, imageCreate;
-
+    ListView lvLista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageSearch = (ImageButton) findViewById(R.id.photo_search);
-        imageContact = (ImageButton) findViewById(R.id.photo_contact);
-        imagePhone = (ImageButton) findViewById(R.id.photo_phone);
-        imageCreate = (ImageButton) findViewById(R.id.photo_create);
+        lvLista = (ListView) findViewById(R.id.listview_with_fab);
 
-        imageSearch.setOnClickListener(this);
-        imageContact.setOnClickListener(this);
-        imagePhone.setOnClickListener(this);
-        imageCreate.setOnClickListener(this);
+        String[] listItwms = new String[]{"Prueba1", "Prueba2", "Prueba3", "Prueba4",
+                "Prueba5", "Prueba6", "Prueba7", "Prueba8", "Prueba9",
+                "Prueba10"
+        };
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, listItwms);
+        lvLista.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floating_action_button_fab_with_listview);
+        fab.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.photo_contact:
-                Intent intent = new Intent(this, ContactoDetalle.class);
-                startActivity(intent);
-                break;
-
-            case R.id.photo_create:
-                Intent intent2 = new Intent(this, CrearContacto.class);
-                startActivity(intent2);
-                break;
-
-            case R.id.photo_search:
-                Toast.makeText(this.getApplicationContext(), "Función de busqueda, no se encontró al usuario", Toast.LENGTH_LONG).show();
-                break;
-
-            case R.id.photo_phone:
-                Toast.makeText(this.getApplicationContext(), "Función de llamada al numero: 38499583", Toast.LENGTH_LONG).show();
-                break;
-        }
-
+        Intent intent = new Intent();
+        startActivity(intent);
     }
 }
