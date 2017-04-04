6cdd87130142eb250a3435a5c30a578a9faee482
Sergio Sánchez <ssanchez@accionti.com>
2017-04-04 15:38:25 -0500

    Uso de sesiones en la app

--- RACM/app/src/main/java/itesm/mx/racm/RegistrarPerfil.java
+++ RACM/app/src/main/java/itesm/mx/racm/RegistrarPerfil.java
@@ -1,25 +1,55 @@
 package itesm.mx.racm;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 
 public class RegistrarPerfil extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnGuardar, btnDireccion;
-    ImageButton btnImagePhoto;
+    private Button btnGuardar, btnDireccion;
+    private EditText editNombre, editTelefono, editDireccion;
+    private ImageButton btnImagePhoto;
+    private SessionManager session;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_perfil);
-        Button btnGuardar = (Button) findViewById(R.id.button_guardarRP);
+
+
+        editNombre = (EditText) findViewById(R.id.edit_nombreRP);
+        editDireccion = (EditText) findViewById(R.id.edit_direccionRP);
+        editTelefono = (EditText) findViewById(R.id.edit_telefonoRP);
+
+        btnGuardar = (Button) findViewById(R.id.button_guardarRP);
+        btnDireccion = (Button) findViewById(R.id.button_ubicacionRP);
+        btnImagePhoto = (ImageButton) findViewById(R.id.image_fotoRP);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+        session = new SessionManager(getApplicationContext());
+
+        if(session.isLoggedIn()){
+            Intent intent = new Intent(RegistrarPerfil.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
         btnGuardar.setOnClickListener(this);
+        btnImagePhoto.setOnClickListener(this);
+
+
+
     }
 
 
@@ -27,8 +57,14 @@ public class RegistrarPerfil extends AppCompatActivity implements View.OnClickLi
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button_guardarRP:
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
+                String name = editNombre.getText().toString().trim();
+                String phone = editTelefono.getText().toString().trim();
+                String adress = editDireccion.getText().toString().trim();
+
+                if(!name.isEmpty() && !adress.isEmpty() && !phone.isEmpty()){
+                    login(name, phone, adress);
+                }
+
                 break;
 
             case  R.id.image_fotoRP:
@@ -42,5 +78,26 @@ public class RegistrarPerfil extends AppCompatActivity implements View.OnClickLi
         }
 
 
+    }
+
+    private void login(final String name, final String phone, final String adress){
+        String tag_string_req = "req_login";
+
+        pDialog.setMessage("Registrando in ...");
+        showDialog();
+
+
+
+
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
