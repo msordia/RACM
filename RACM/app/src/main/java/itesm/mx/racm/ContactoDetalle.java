00c7545abbae4754416859f94a30d64867a4106c
Ricardo <ricardotrevino24@gmail.com>
2017-04-27 10:43:56 -0500

    Agrega findContacto y TextViews en ContactoDetalle

--- RACM/app/src/main/java/itesm/mx/racm/ContactoDetalle.java
+++ RACM/app/src/main/java/itesm/mx/racm/ContactoDetalle.java
@@ -1,16 +1,28 @@
 package itesm.mx.racm;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import itesm.mx.racm.datos.Contacto;
+import itesm.mx.racm.datos.ContactoOperations;
 
 public class ContactoDetalle extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton imageFoto, imageModify, imagePhone, imageIcons;
     MenuFragment fragmentoMenu;
+    long idContacto;
+    ContactoOperations dao;
+    TextView tvNombre, tvTelefono;
+    Bitmap imageBitmap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,8 @@ public class ContactoDetalle extends AppCompatActivity implements View.OnClickLi
 
         fragmentoMenu = (MenuFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_menu);
 
+        tvNombre = (TextView) findViewById(R.id.text_nombreCD);
+        tvTelefono = (TextView) findViewById(R.id.text_telefonoCD);
         imageFoto = (ImageButton) findViewById(R.id.image_fotoPD);
         imageModify = (ImageButton) findViewById(R.id.photo_modify);
         imagePhone = (ImageButton) findViewById(R.id.photo_phone);
@@ -28,6 +42,25 @@ public class ContactoDetalle extends AppCompatActivity implements View.OnClickLi
         imageModify.setOnClickListener(this);
         imagePhone.setOnClickListener(this);
         imageIcons.setOnClickListener(this);
+
+        //Get intent extras "idContacto"
+
+        dao = new ContactoOperations(this);
+        dao.open();
+
+        Bundle bundle= getIntent().getExtras();
+        if(bundle!=null){
+           idContacto = bundle.getLong("idContacto");
+            Log.d("Este es el id", String.valueOf(idContacto));
+            Contacto contactoPrueba;
+            contactoPrueba = dao.findContacto(idContacto);
+            Log.d("Nombre", contactoPrueba.getNombre());
+            //Cambiar imageFoto,tvNombre y tvTelefono
+            imageBitmap = BitmapFactory.decodeByteArray(contactoPrueba.getFoto(), 0, contactoPrueba.getFoto().length);
+            imageFoto.setImageBitmap(imageBitmap);
+            tvNombre.setText(contactoPrueba.getNombre());
+            tvTelefono.setText(contactoPrueba.getTelefono());
+        }
 
     }
 
